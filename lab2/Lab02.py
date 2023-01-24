fe542316c589b253970b4a69cce56a7428be6c3d
sarvin <sraeis2@myseneca.ca>
2023-01-24 15:42:30 -0500

    fixed the new window bug, added fps calculator

--- lab2/Lab02.py
+++ lab2/Lab02.py
@@ -1,6 +1,5 @@
 import cv2 as cv
-import time
-
+import time 
 PINK = [174,55,255]
 
 # Start a video capture, using device's camera
@@ -16,7 +15,7 @@ frame_height = int(cap.get(4))
 print("Frame width: " , frame_width)
 print("Frame height: " , frame_height)
 
-counter = 0
+snapshotCount = 0
 # Read until video is completed
 while(cap.isOpened()):
     # Capture frame-by-frame
@@ -27,7 +26,10 @@ while(cap.isOpened()):
     # Display the frame
     cv.imshow('frame',frame)
     key = cv.waitKey(25)
-    
+
+    # calculate frames per second
+    fps = cap.get(cv.CAP_PROP_FPS)
+    print("Frames per second: {0}".format(fps))
 
     # Press Q or ESC on keyboard to exit
     if key%256 == ord('q') or key%256 == 27:
@@ -36,19 +38,20 @@ while(cap.isOpened()):
    
     
     # press x on keyboard to take a screenshot. crops and adds border to the screenshot and saves it.
-    if key & 0xFF == ord('x'):
+    elif key & 0xFF == ord('x'):
         
-        img = f'image{counter}.png'
-        modified_img = frame[30 : 360, 30 : 640 ]
-        modified_img= cv.copyMakeBorder(modified_img,50,50,50,50,cv.BORDER_CONSTANT,value=PINK)
-        cv.imwrite(img, modified_img) 
-
-        read_img = cv.imread(img)
-        cv.namedWindow(img)
-        cv.imshow(img, read_img)
-        time.sleep(1)
-        cv.destroyWindow(img)
-        counter += 1
+        snapshotName = "image{}.jpeg".format(snapshotCount)
+        croppedFrame = frame[30:690, 30:1250]
+        color = [54, 54, 171]
+        paddedFrame = cv.copyMakeBorder(croppedFrame, 50,50, 50, 50, cv.BORDER_CONSTANT,value=color)
+        cv.imwrite(snapshotName, paddedFrame)
+        print(snapshotName+" created!")
+        img = cv.imread(snapshotName, 1)
+        cv.imshow('image', img)
+        cv.waitKey(3000)
+        cv.destroyWindow('image')
+        print('window destroyed')
+        snapshotCount+=1
 
           
         
@@ -56,3 +59,6 @@ while(cap.isOpened()):
 
 # Release the video capture 
 cap.release()
+
+# close all window frames
+cv.destroyAllWindows()
