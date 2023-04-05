4d0531fa8e90bd539e501d30c52c8d174c421d5f
ianmhenriquez <ianmhenriquez@hotmail.com>
2023-04-04 20:36:19 -0400

    added xml and fixes to trainer

--- project/trainer.py
+++ project/trainer.py
@@ -3,23 +3,38 @@ import numpy as np
 import os
 
 # Step 1: Generate positive and negative sample lists
-pos_images_dir = "./positive"
-neg_images_dir = "./negative"
-pos_list_file = "./pos_list.txt"
-neg_list_file = "./neg_list.txt"
+pos_images_dir = "positive"
+neg_images_dir = "negative"
+pos_list_file = "pos_list.txt"
+neg_list_file = "neg_list.txt"
+
+#display position of pos_list_file
 
 with open(pos_list_file, "w") as f:
     for img_file in os.listdir(pos_images_dir):
-        f.write(os.path.join(pos_images_dir, img_file) + "\n")
+        f.write(os.path.join(pos_images_dir, img_file) + " 1 " + "5 " + "5 " + "480 " + "190" "\n")
 
 with open(neg_list_file, "w") as f:
     for img_file in os.listdir(neg_images_dir):
         f.write(os.path.join(neg_images_dir, img_file) + "\n")
 
+
+
 # Step 2: Create a vector file
-vec_file = "./pos_vec.vec"
+vec_file = "pos_vec.vec"
 pos_samples = len(os.listdir(pos_images_dir))
 width, height = 64, 64
 
 cmd = f"opencv_createsamples -info {pos_list_file} -num {pos_samples} -w {width} -h {height} -vec {vec_file}"
+os.system(cmd)
+
+# Step 3: Train the classifier
+data_dir = "data"
+bg_file = "neg_list.txt"
+num_pos = 56
+num_neg = 340
+num_stages = 10
+vec_file = "pos_vec.vec"
+
+cmd = f"opencv_traincascade -data {data_dir} -vec {vec_file} -bg {bg_file} -numPos {num_pos} -numNeg {num_neg} -numStages {num_stages} -w {width} -h {height} -featureType LBP"
 os.system(cmd)
