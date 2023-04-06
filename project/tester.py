19c8dec91bb4da5a47d55c75067a162b2431250e
ianmhenriquez <ianmhenriquez@hotmail.com>
2023-04-06 17:11:52 -0400

    made more adjustments to classifier

--- project/tester.py
+++ project/tester.py
@@ -7,11 +7,12 @@ classifier = "data/cascade.xml"
 img = cv.imread(test_image)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 classifier = cv.CascadeClassifier(classifier)
-faces = classifier.detectMultiScale(gray, 1.3, 5)
+cars = classifier.detectMultiScale(gray, 2.9, 1)
 
-for (x, y, w, h) in faces:
-    cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+for (x, y, w, h) in cars:
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
 
-cv.imshow("Face Detection", img)
+
+cv.imshow("Car Detection", img)
 cv.waitKey(0)
 cv.destroyAllWindows()
