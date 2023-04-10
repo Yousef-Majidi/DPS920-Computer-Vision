dfd831fa84e66a2d08ca9bef12884997c26d822b
Yousef <y.majidin@gmail.com>
2023-04-10 12:34:37 -0400

    finished tester.py

--- project/tester.py
+++ project/tester.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 # Load the pre-trained Haar Cascade classifier for car detection
 car_cascade = cv2.CascadeClassifier('data/cascade.xml')
@@ -9,18 +10,28 @@ for var in range(1, 30):
 
     if image is None:
         continue
-    # image = cv2.resize(image, (300, 170))
 
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Detect cars in the image using the Haar Cascade classifier
-    cars = car_cascade.detectMultiScale(
-        gray, scaleFactor=1.001, minNeighbors=5, minSize=(100, 100))
+    # detect the car in the image using the Haar Cascade classifier
+    cars = car_cascade.detectMultiScale(gray, 1.001, 5, 0, (50, 50))
 
-    # Draw rectangles around the cars detected in the image
-    for (x, y, w, h) in cars:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # print the number of cars detected
+    print('Number of cars detected: ' + str(len(cars)))
+
+   # apply non-maximum suppression to remove overlapping bounding boxes
+    indices = cv2.dnn.NMSBoxes(cars, [1.0]*len(cars), 0.9, 0.01)
+
+    # sort the indices in descending order based on the confidence scores
+    indices_sorted = sorted(
+        range(len(cars)), key=lambda i: cars[i][2], reverse=True)
+
+    # draw a rectangle around the most confident car
+    if len(indices_sorted) > 0:
+        i = indices_sorted[0]
+        (x, y, w, h) = cars[i]
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Display the image with the rectangles around the cars detected
     cv2.imshow('Car Detection', image)
